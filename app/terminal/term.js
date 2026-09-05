3092871ff9029b81f42ed5673b1b9067064d2561
Owen Allen <allen_owen2@icloud.com>
2026-09-05 15:36:12 +0000

    potentially fixed a writeUTF16 bug in term.js

--- app/terminal/term.js
+++ app/terminal/term.js
@@ -62,9 +62,8 @@ function syncProp(name, value) {
     if (oldProps[name] !== value)
         native.propUpdate(name, value);
 }
-let decoder = new TextDecoder();
 exports.write = (data) => {
-    term.io.writeUTF16(decoder.decode(lib.codec.stringToCodeUnitArray(data)));
+    term.io.writeUTF16(data);
     syncProp('applicationCursor', term.keyboard.applicationCursor);
 };
 term.io.sendString = term.io.onVTKeyStroke = (data) => {
