93a4118949ccd540155afc32623c127c512dc521
Owen Allen <allen_owen2@icloud.com>
2026-09-08 15:28:30 +0000

    bounds and consistency checks as well as error codes in platform.h
    No actual bug fixes, just bounds and consistency checks as well as error codes instead of silent failures for security reasons

--- platform/platform.h
+++ platform/platform.h
@@ -2,6 +2,14 @@
 #define PLATFORM_H
 #include "misc.h"
 
+// Error codes for platform functions
+typedef enum {
+    PLATFORM_OK = 0,
+    PLATFORM_ERR_READ_FAILED = 1,
+    PLATFORM_ERR_INVALID_DATA = 2,
+    PLATFORM_ERR_PERMISSION = 3
+} platform_error_t;
+
 // for some reason a tick is always 10ms
 struct cpu_usage {
     uint64_t user_ticks;
@@ -9,7 +17,8 @@ struct cpu_usage {
     uint64_t idle_ticks;
     uint64_t nice_ticks;
 };
-struct cpu_usage get_cpu_usage(void);
+
+platform_error_t get_cpu_usage(struct cpu_usage *usage);
 
 struct mem_usage {
     uint64_t total;
@@ -17,12 +26,17 @@ struct mem_usage {
     uint64_t active;
     uint64_t inactive;
 };
-struct mem_usage get_mem_usage(void);
+
+platform_error_t get_mem_usage(struct mem_usage *usage);
 
 struct uptime_info {
     uint64_t uptime_ticks;
     uint64_t load_1m, load_5m, load_15m;
 };
-struct uptime_info get_uptime(void);
+
+platform_error_t get_uptime(struct uptime_info *info);
+
+int validate_cpu_usage(const struct cpu_usage *usage);
+int validate_mem_usage(const struct mem_usage *usage);
 
 #endif
