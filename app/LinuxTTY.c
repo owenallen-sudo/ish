87a1b85e37a0092d287ff290acd7ab45701d3cad
Owen Allen <allen_owen2@icloud.com>
2026-09-08 17:50:00 +0000

    Several potential major bugs and vulnerability fixes in app/LinuxTTY.c
    Potentially fixed race conditions. Added null checks for tty, tty->port, client_data and len in ios_tty_write. Added a null pointer in ios_tty_cb_resize. Fixed an integer underflow in ios_tty_console_write. Added an error check in tty_alloc_driver. Added length validation to prevent zero-length operations in ios_tty_cb_send_input. Added null checks in ios_tty_write_room. Null checks for tty, data and len in ios_tty_console_write.  Fix for an objective c object leak.

--- app/LinuxTTY.c
+++ app/LinuxTTY.c
@@ -42,18 +42,26 @@ static struct ios_tty ios_ttys[NUM_TTYS];
 
 static int ios_tty_port_activate(struct tty_port *port, struct tty_struct *tty) {
     BUG_ON(port != &ios_ttys[tty->index].port);
-    port->client_data = (void *) Terminal_terminalWithType_number(TTY_MAJOR, tty->index);
+    nsobj_t terminal = Terminal_terminalWithType_number(TTY_MAJOR, tty->index);
+    if (!terminal)
+        return -ENOMEM;
+    port->client_data = (void *) terminal;
     Terminal_setLinuxTTY(port->client_data, &container_of(port, struct ios_tty, port)->linux_tty);
     return 0;
 }
+
 static void ios_tty_port_destruct(struct tty_port *port) {
-    async_do_in_ios(^{
-        objc_put(port->client_data);
-        async_do_in_irq(^{
-            kfree(port);
+    nsobj_t client_data = port->client_data;
+    if (client_data) {
+        async_do_in_ios(^{
+            objc_put(client_data);
+            async_do_in_irq(^{
+                tty_port_put(port);
+            });
         });
-    });
+    }
 }
+
 static struct tty_port_operations ios_tty_port_ops = {
     .activate = ios_tty_port_activate,
     .destruct = ios_tty_port_destruct,
@@ -66,21 +74,26 @@ static void ios_tty_cb_can_output(struct linux_tty *linux_tty) {
 
 static void ios_tty_cb_send_input(struct linux_tty *linux_tty, const char *data, size_t length) {
     struct ios_tty *tty = container_of(linux_tty, struct ios_tty, linux_tty);
-    tty_insert_flip_string(&tty->port, data, length);
-    tty_flip_buffer_push(&tty->port);
+    if (length > 0) {
+        tty_insert_flip_string(&tty->port, data, length);
+        tty_flip_buffer_push(&tty->port);
+    }
 }
 
 static void ios_tty_cb_resize(struct linux_tty *linux_tty, int cols, int rows) {
     struct ios_tty *tty = container_of(linux_tty, struct ios_tty, linux_tty);
-    struct winsize ws = {
-        .ws_row = rows,
-        .ws_col = cols,
-    };
-    tty_do_resize(tty->port.tty, &ws);
+    struct tty_struct *tty_struct = tty_port_tty_get(&tty->port);
+    if (tty_struct) {
+        struct winsize ws = {
+            .ws_row = rows,
+            .ws_col = cols,
+        };
+        tty_do_resize(tty_struct, &ws);
+        tty_kref_put(tty_struct);
+    }
 }
 
 static void ios_tty_cb_hangup(struct linux_tty *linux_tty) {
-    // nah
 }
 
 static struct linux_tty_callbacks ios_tty_callbacks = {
@@ -95,10 +108,14 @@ static int ios_tty_open(struct tty_struct *tty, struct file *filp) {
 }
 
 static int ios_tty_write(struct tty_struct *tty, const unsigned char *data, int len) {
+    if (!tty || !tty->port || !tty->port->client_data || len <= 0)
+        return 0;
     return Terminal_sendOutput_length(tty->port->client_data, data, len);
 }
 
 static unsigned int ios_tty_write_room(struct tty_struct *tty) {
+    if (!tty || !tty->port || !tty->port->client_data)
+        return 0;
     return Terminal_roomForOutput(tty->port->client_data);
 }
 
@@ -109,18 +126,27 @@ static struct tty_operations ios_tty_ops = {
 };
 
 static int ios_tty_console_setup(struct console *console, char *what) {
-    console->data = (void *) Terminal_terminalWithType_number(TTY_MAJOR, 1);
+    nsobj_t terminal = Terminal_terminalWithType_number(TTY_MAJOR, 1);
+    if (!terminal)
+        return -ENOMEM;
+    if (console->data)
+        objc_put(console->data);
+    console->data = (void *) terminal;
     return 0;
 }
 
 static void ios_tty_console_write(struct console *console, const char *data, unsigned len) {
     nsobj_t tty = console->data;
+    if (!tty || !data || len == 0)
+        return;
+    
     while (len) {
         const char *newline = memchr(data, '\n', len);
         if (newline != NULL) {
-            Terminal_sendOutput_length(tty, data, newline - data);
+            size_t chunk_len = newline - data;
+            Terminal_sendOutput_length(tty, data, chunk_len);
             Terminal_sendOutput_length(tty, "\r\n", 2);
-            len -= newline - data + 1;
+            len -= chunk_len + 1;
             data = newline + 1;
         } else {
             Terminal_sendOutput_length(tty, data, len);
@@ -151,6 +177,10 @@ static __init int ios_tty_init(void) {
     }
 
     ios_tty_driver = tty_alloc_driver(NUM_TTYS, TTY_DRIVER_REAL_RAW | TTY_DRIVER_RESET_TERMIOS);
+    if (!ios_tty_driver) {
+        panic("ios tty: failed to tty_alloc_driver");
+    }
+    
     ios_tty_driver->driver_name = "ios";
     ios_tty_driver->name = "tty";
     ios_tty_driver->name_base = 1;
@@ -165,8 +195,9 @@ static __init int ios_tty_init(void) {
         tty_port_link_device(&ios_ttys[i].port, ios_tty_driver, i);
     }
 
-    if (tty_register_driver(ios_tty_driver))
+    if (tty_register_driver(ios_tty_driver)) {
         panic("ios tty: failed to tty_register_driver");
+    }
 
     register_console(&ios_tty_console);
 
